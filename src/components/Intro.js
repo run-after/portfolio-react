6ff2ed7baaf1dcc0eba529d9d803002f763bdeb5
run-after <run.after@protonmail.com>
2021-10-07 13:33:49 -0500

    Change links in intro section from image to react-icons

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,7 @@
 import '../styles/Intro.css';
-import githubIcon from '../media/github.png';
-import emailIcon from '../media/email.png';
-import resumeIcon from '../media/cv.png';
 import resume from '../media/ChaseStearn.pdf';
+import { GoMail } from 'react-icons/go';
+import { AiOutlineSolution, AiOutlineGithub } from 'react-icons/ai';
 
 
 const Intro = () => {
@@ -11,9 +10,15 @@ const Intro = () => {
       <h1 className='greeting'>Hi, I'm <span className='name'>Chase</span>.</h1>
       <p className='description'>I'm a self-taught FullStack Developer</p>
       <div className='links'>
-        <a href='https://github.com/run-after' target='_blank' rel='noreferrer'><img className='link' src={githubIcon} alt='link to github' /></a>
-        <a href='mailto:run.after@protonmail.com'><img className='link' src={emailIcon} alt='link to email' /></a>
-        <a href={resume} target='_blank' rel='noreferrer'><img className='link' src={resumeIcon} alt='link to cv' /></a>
+        <a href='https://github.com/run-after' target='_blank' rel='noreferrer'>
+          <AiOutlineGithub className='link'/>
+        </a>
+        <a href='mailto:run.after@protonmail.com'>
+          <GoMail className='link'/>
+        </a>
+        <a href={resume} target='_blank' rel='noreferrer'>
+          <AiOutlineSolution className='link' />
+        </a>
       </div>
       
     </div>
