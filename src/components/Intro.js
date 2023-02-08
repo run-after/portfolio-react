2e19ac961eebfe06a915707b835385eae8a97a94
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-08 15:04:20 -0500

    Remove self-taught

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,26 +1,26 @@
-import '../styles/Intro.css';
-import resume from '../media/ChaseStearn.pdf';
-import { GoMail } from 'react-icons/go';
-import { AiOutlineSolution, AiOutlineGithub } from 'react-icons/ai';
-
+import "../styles/Intro.css";
+import resume from "../media/ChaseStearn.pdf";
+import { GoMail } from "react-icons/go";
+import { AiOutlineSolution, AiOutlineGithub } from "react-icons/ai";
 
 const Intro = () => {
   return (
-    <div id='intro'>
-      <h1 className='greeting'>Hi, I'm <span className='name'>Chase</span>.</h1>
-      <p className='description'>I'm a self-taught FullStack Developer</p>
-      <div className='links'>
-        <a href='https://github.com/run-after' target='_blank' rel='noreferrer'>
-          <AiOutlineGithub className='link'/>
+    <div id="intro">
+      <h1 className="greeting">
+        Hi, I'm <span className="name">Chase</span>.
+      </h1>
+      <p className="description">I'm a JavaScript Developer</p>
+      <div className="links">
+        <a href="https://github.com/run-after" target="_blank" rel="noreferrer">
+          <AiOutlineGithub className="link" />
         </a>
-        <a href='mailto:run.after@protonmail.com'>
-          <GoMail className='link'/>
+        <a href="mailto:run.after@protonmail.com">
+          <GoMail className="link" />
         </a>
-        <a href={resume} target='_blank' rel='noreferrer'>
-          <AiOutlineSolution className='link' />
+        <a href={resume} target="_blank" rel="noreferrer">
+          <AiOutlineSolution className="link" />
         </a>
       </div>
-      
     </div>
   );
 };
